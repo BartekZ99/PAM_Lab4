9d6bc711530c8fa6b0cf8c08ba73fa988e556368
Bartosz Zmysłowski <zmyslowski.bartosz@Interia.pl>
2021-10-23 17:26:42 +0200

    Dokonczone lab. 3

--- app/src/main/java/com/example/myfrags/MainActivity.java
+++ app/src/main/java/com/example/myfrags/MainActivity.java
@@ -9,17 +9,24 @@ import androidx.fragment.app.FragmentTransaction;
 
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.PrimitiveIterator;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements Fragment1.OnButtonClickListener{
 
    // private FragmentManager fragmentManager;
    // private Fragment fragment1, fragment2, fragment3, fragment4;
 
     private int[] frames;
     private boolean hiden;
+    private int[] sequence; //tablica przechowująca sekwencję fragmentów
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,28 +36,39 @@ public class MainActivity extends FragmentActivity {
         if(savedInstanceState == null){
             frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
             hiden = false;
+
+            sequence = new int[]{0,1,2,3}; //początkowa sekwencja fragmentów
+
+            Fragment[] fragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            for(int i = 0; i < 4; i++){
+                transaction.add(frames[i], fragments[i]);
+            }
+            transaction.addToBackStack(null);
+            transaction.commit();
         } else {
             frames = savedInstanceState.getIntArray("FRAMES");
             hiden = savedInstanceState.getBoolean("HIDEN");
+
+            sequence = savedInstanceState.getIntArray("SEQUENCE");
         }
+    }
 
-        /*
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-        fragment4 = new Fragment4();
+    private void newFragments(){
+        Fragment[] newFragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
 
-        fragmentManager = getSupportFragmentManager();
+        Fragment[] inSequence = new Fragment[]{newFragments[sequence[0]], newFragments[sequence[1]], newFragments[sequence[2]], newFragments[sequence[3]]};
+        newFragments = inSequence;
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frame1, fragment1);
-        transaction.add(R.id.frame2, fragment2);
-        transaction.add(R.id.frame3, fragment3);
-        transaction.add(R.id.frame4, fragment4);
+        for(int i = 0; i < 4; i++){
+            transaction.replace(frames[i], newFragments[i]);
+            if(hiden && !(newFragments[i] instanceof Fragment1)) transaction.hide(newFragments[i]);
+        }
         transaction.addToBackStack(null);
         transaction.commit();
-        */
-
     }
 
     @Override
@@ -59,5 +77,78 @@ public class MainActivity extends FragmentActivity {
 
         outState.putIntArray("FRAMES", frames);
         outState.putBoolean("HIDEN", hiden);
+
+        outState.putIntArray("SEQUENCE", sequence);
+    }
+
+    @Override
+    public void onButtonClickShuffle() {
+       /* List<Integer> list = new ArrayList<Integer>(Arrays.asList(frames[0], frames[1], frames[2], frames[3]));
+        Collections.shuffle(list);
+        for(int i = 0; i < 4; i++){
+            frames[i] = list.get(i).intValue();
+        }
+        newFragments();*/
+        //Toast.makeText(getApplicationContext(), "Shuffle", Toast.LENGTH_SHORT).show();
+
+        List<Integer> s = new ArrayList<>(Arrays.asList(sequence[0], sequence[1], sequence[2], sequence[3]));
+        Collections.shuffle(s);
+        for(int i = 0; i < 4; i++) sequence[i] = s.get(i);
+        newFragments();
+    }
+
+    @Override
+    public void onButtonClickClockwise() {
+        int t = frames[0];
+        frames[0] = frames[1];
+        frames[1] = frames[2];
+        frames[2] = frames[3];
+        frames[3] = t;
+
+        newFragments();
+
+        //Toast.makeText(getApplicationContext(), "Clockwise", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onButtonClickHide() {
+        if(hiden) return;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        for(Fragment f : fragmentManager.getFragments()){
+            if(f instanceof Fragment1) continue;
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.hide(f);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        hiden = true;
+
+        //Toast.makeText(getApplicationContext(), "Hide", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onButtonClickRestore() {
+        if(!hiden) return;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        for(Fragment f : fragmentManager.getFragments()){
+            if(f instanceof Fragment1) continue;
+            transaction.show(f);
+        }
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        hiden = false;
+
+       // Toast.makeText(getApplicationContext(), "Restore", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment){
+        super.onAttachFragment(fragment);
+
+        if(fragment instanceof Fragment1){
+            ((Fragment1) fragment).setOnButtonClickListener(this);
+        }
     }
 }
